7381b2314989f1b88cfc409c0c87f951c91a9bb6
taco <tacochen@lips-hci.com>
2021-02-13 02:05:15 +0800

    pass but very slow

--- 826.most-profit-assigning-work.cpp
+++ 826.most-profit-assigning-work.cpp
@@ -7,9 +7,63 @@
 
 // @lc code=start
 class Solution {
+    struct Worker
+    {
+        /* data */
+        int id,power;
+    };
+    struct Work
+    {
+        int difficulty, profit;
+        Work(int difficulty, int profit)
+        {
+            this->difficulty = difficulty;
+            this->profit = profit;
+        }
+    };
+    static bool compareWork(Work &i,Work &j)
+    {
+        return i.profit < j.profit;
+    }
+    bool compareWorkDif(Work &i,Work &j)
+    {
+        return i.difficulty < j.difficulty;
+    }    
+    
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        
+        int minStartWork = 0;
+        int points  = 0;
+        vector<Work> work;
+        work.push_back(Work(0,0));
+        for (size_t i = 0; i < profit.size(); i++)
+        {
+            work.push_back(Work(difficulty[i],profit[i]));
+        }
+        sort(work.begin(),work.end(),compareWork);
+        sort(worker.begin(),worker.end());
+        for (size_t i = 0; i < worker.size(); i++)
+        {
+            // at least pick the hardest one
+            for (size_t j = work.size()-1; j > minStartWork; j--)
+            {
+                /* code */
+                if (worker[i] < work[j].difficulty)
+                {
+                    // can't do, keep try
+                }
+                else
+                {
+                    // can do
+                    minStartWork = j;
+                    break;
+                }  
+            }
+            //
+            points += work[minStartWork].profit;
+        }
+        return points;
+
     }
 };
 // @lc code=end
