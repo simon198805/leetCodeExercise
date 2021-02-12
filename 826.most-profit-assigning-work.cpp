0b75a24928d9d849369d003be7b9616d559c4bad
taco <tacochen@lips-hci.com>
2021-02-12 13:20:33 +0800

    start 826

---
+++ 826.most-profit-assigning-work.cpp
@@ -0,0 +1,16 @@
+# include "include.hpp"
+/*
+ * @lc app=leetcode id=826 lang=cpp
+ *
+ * [826] Most Profit Assigning Work
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        
+    }
+};
+// @lc code=end
+
