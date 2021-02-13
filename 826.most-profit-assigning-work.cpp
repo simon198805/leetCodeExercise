df26eb2254c19be0e587328483b031be393b19e5
taco <tacochen@lips-hci.com>
2021-02-13 15:27:36 +0800

    the key is : don't renew the heighest profit this worker may achive

--- 826.most-profit-assigning-work.cpp
+++ 826.most-profit-assigning-work.cpp
@@ -7,51 +7,28 @@
 
 // @lc code=start
 class Solution {
-    struct Work
-    {
-        int difficulty, profit;
-        Work(int &difficulty, int &profit)
-        {
-            this->difficulty = difficulty;
-            this->profit = profit;
-        }
-    };
-    static inline bool compareWork(const Work &i,const Work &j)
-    {
-        return i.profit < j.profit;
-    }
-    
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        int minStartWork = 0;
         int points  = 0;
-        vector<Work> work;
-        int zero = 0;
-        work.push_back(Work(zero,zero));
+        vector<pair<int,int>> work;
+        work.push_back({0,0});
         for (size_t i = 0; i < profit.size(); i++)
         {
-            work.push_back(Work(difficulty[i],profit[i]));
+            work.push_back({profit[i], difficulty[i]});
+            //cout << "w" << profit[i] << "," << difficulty[i] << endl;
         }
-        sort(work.begin(),work.end(),compareWork);
+        sort(work.begin(),work.end());
         sort(worker.begin(),worker.end());
-        const int workerSize = worker.size();
-        const int workSize = work.size();
+        int i = worker.size() -1 ;
+        int workSize = work.size()-1;
 
-        for (size_t i = 0; i < workerSize; i++)
+        for (; i >=0 ; i--)
         {
-            // at least pick the hardest one
-            for (size_t j = workSize-1; j > minStartWork; j--)
-            {
-                /* code */
-                if (worker[i] >= work[j].difficulty)
-                {
-                    // can do
-                    minStartWork = j;
-                    break;
-                }  
-            }
-            //
-            points += work[minStartWork].profit;
+            int j = workSize;
+            const int wp = worker[i];
+            while (wp < work[j].second && j > 0) j--;
+            if (j == 0) break;
+            points += work[j].first;
         }
         return points;
 
