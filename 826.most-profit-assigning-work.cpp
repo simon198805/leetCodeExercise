8a238ad95b55816a4bebf4f21c8699a852df8c84
taco <tacochen@lips-hci.com>
2021-02-13 14:12:22 +0800

    modify some minor parts

--- 826.most-profit-assigning-work.cpp
+++ 826.most-profit-assigning-work.cpp
@@ -7,52 +7,43 @@
 
 // @lc code=start
 class Solution {
-    struct Worker
-    {
-        /* data */
-        int id,power;
-    };
     struct Work
     {
         int difficulty, profit;
-        Work(int difficulty, int profit)
+        Work(int &difficulty, int &profit)
         {
             this->difficulty = difficulty;
             this->profit = profit;
         }
     };
-    static bool compareWork(Work &i,Work &j)
+    static inline bool compareWork(const Work &i,const Work &j)
     {
         return i.profit < j.profit;
     }
-    bool compareWorkDif(Work &i,Work &j)
-    {
-        return i.difficulty < j.difficulty;
-    }    
     
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         int minStartWork = 0;
         int points  = 0;
         vector<Work> work;
-        work.push_back(Work(0,0));
+        int zero = 0;
+        work.push_back(Work(zero,zero));
         for (size_t i = 0; i < profit.size(); i++)
         {
             work.push_back(Work(difficulty[i],profit[i]));
         }
         sort(work.begin(),work.end(),compareWork);
         sort(worker.begin(),worker.end());
-        for (size_t i = 0; i < worker.size(); i++)
+        const int workerSize = worker.size();
+        const int workSize = work.size();
+
+        for (size_t i = 0; i < workerSize; i++)
         {
             // at least pick the hardest one
-            for (size_t j = work.size()-1; j > minStartWork; j--)
+            for (size_t j = workSize-1; j > minStartWork; j--)
             {
                 /* code */
-                if (worker[i] < work[j].difficulty)
-                {
-                    // can't do, keep try
-                }
-                else
+                if (worker[i] >= work[j].difficulty)
                 {
                     // can do
                     minStartWork = j;
