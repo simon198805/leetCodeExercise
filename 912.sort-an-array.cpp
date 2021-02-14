476476843db78fd5ab51736aaaa5f9533702aa09
taco <tacochen@lips-hci.com>
2021-02-14 21:14:04 +0800

    solve 912

---
+++ 912.sort-an-array.cpp
@@ -0,0 +1,17 @@
+#include "include.hpp"
+/*
+ * @lc app=leetcode id=912 lang=cpp
+ *
+ * [912] Sort an Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
+// @lc code=end
+
